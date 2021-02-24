540c15a4985abec146bd1c66a2d3307ff89b0b33
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2021-02-24 00:20:00 -0300

    Improving Next.js

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import next from "next";
+
+// Styles
+import styles from '../styles/components/Profile.module.css';
+
+export default function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/adonaipinheiro.png" alt="Adonai Pinheiro" />
+      <div>
+        <strong>Adonai Pinheiro</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+
+// Components
+import CompletedChallenges from "../components/CompletedChallenges";
+import CountDown from "../components/CountDown";
+import ExperienceBar from "../components/ExperienceBar";
+import Profile from "../components/Profile";
+
+// Styles
+import styles from '../styles/pages/Home.module.css';
+
+export default function App() {
+  return (
+    <div className={styles.container}>
+      <Head><title>Início | move.it</title></Head>
+      <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  )
+}
